af11b9f5a3b6dd237e93b3c4bed290f08da1d3ae
sirvivor32 <kamub79@gmail.com>
2022-02-12 10:24:29 +0530

    changes

--- website/models.py
+++ website/models.py
@@ -5,12 +5,12 @@ from sqlalchemy.sql import func
 
 class Note(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   data = db.Column(db.String(15000))
+   data = db.Column(db.String(15000000000))
    date = db.Column(db.DateTime(timezone=True), default=func.now())
    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(150000000000))
     notes = db.relationship('Note')
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template,Request,flash,redirect,url_for
-
+from flask_login import login_required, login_user, logout_user
+from . import db
+from .models import User
 views=Blueprint('views',__name__)
 
 @views.route('/')
+@login_required
 def home():
     return render_template('home.html')
